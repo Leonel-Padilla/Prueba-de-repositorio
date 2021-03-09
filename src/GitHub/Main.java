4e5207b57e8c3552574d4ee47a02354b463bb022
Leonel-Padilla <axcel.sanchez1@ujcv.edu.hn>
2021-03-09 15:04:05 -0600

    Initial commit

---
+++ src/GitHub/Main.java
@@ -0,0 +1,9 @@
+package GitHub;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Proyecto de prueba de repositorio");
+    }
+}
